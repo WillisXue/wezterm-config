57a3352111ef49b626504018778fb4630a6c4676
WillisXue <willis.xue@foxmail.com>
2026-01-22 09:04:19 +0800

    ps

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 $originalPrompt = (Get-Command prompt -ErrorAction SilentlyContinue).ScriptBlock
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
 function Global:prompt {
     if ($env:TERM_PROGRAM -eq "WezTerm") {
